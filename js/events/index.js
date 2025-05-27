bbdef8c51d02d05e2a96c21de99fe45a5c0eef1b
RBcuber <igor.kirichenko.07@gmail.com>
2025-05-28 00:12:54 +0200

    add 2 button

--- js/events/index.js
+++ js/events/index.js
@@ -3,20 +3,33 @@ let bankAccount = {
 	get balance() {
 		return this._balance;
 	},
-	set balance(value) {
-		if (value < 0 && -value > this._balance) {
-			alert('Недостаточно средств');
-		} else {
-			this._balance += value;
-		}
+	set upBalance(value) {
+		this._balance += value
 		document.querySelector(
 			'.balance'
 		).textContent = `На счету ${this._balance}€`;
 	},
+	set withdraw(value) {
+		if (value > this._balance) {
+			alert('Недостаточно средств');
+		} else {
+			this._balance -= value;
+		}
+		document.querySelector(
+			'.balance'
+		).textContent = `На счету ${this._balance}€`;
+	}
 };
-document.querySelector('.bank-button').addEventListener('click', () => {
+document.querySelector('.bank-button1').addEventListener('click', () => {
 	let value = prompt('Введите сумму:');
-	bankAccount.balance = +value;
+	bankAccount.upBalance = +value;
+	if (value === null) {
+		alert(`Ваш текущий баланс: ${bankAccount.balance}€`);
+	}
+});
+document.querySelector('.bank-button2').addEventListener('click', () => {
+	let value = prompt('Введите сумму:');
+	bankAccount.withdraw = +value;
 	if (value === null) {
 		alert(`Ваш текущий баланс: ${bankAccount.balance}€`);
 	}
