5b1d814ae2188261262d4a66c281843a927beeec
RBcuber <igor.kirichenko.07@gmail.com>
2025-05-27 23:15:18 +0200

    create app

---
+++ js/events/index.js
@@ -0,0 +1,21 @@
+let bankAccount = {
+	_balance: 0,
+	get balance() {
+		return this._balance;
+	},
+	set balance(value) {
+		if (value < 0 && -value > this._balance) {
+			alert('Недостаточно средств');
+		} else {
+			this._balance += value;
+		}
+	},
+};
+document.querySelector('.bank-button').addEventListener('click', () => {
+	let value = prompt('Введите сумму:');
+	bankAccount.balance = +value;
+	if(value ===	null){
+		alert(`Ваш текущий баланс: ${bankAccount.balance}€`);
+	}
+	
+});
